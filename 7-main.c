02bc16299a5d4caf605d5df2610333693cd22667
onsjannet <onsjannet@gmail.com>
2020-11-25 19:26:58 +0100

    added filesé

---
+++ 7-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+void print_num(int n)
+{
+    printf("%d\n", n);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
