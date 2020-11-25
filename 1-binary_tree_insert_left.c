d42ef3f27e5721e8b929767843520593a4f5d6a3
onsjannet <onsjannet@gmail.com>
2020-11-25 20:47:17 +0100

    added script that created a node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left -  inserts a node as the left-child of another node
+ * @parent: a pointer to the node to insert the left-child in
+ * @value:  is the value to store in the new node
+ * Return: a pointer to the created node / NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *newNode;
+
+
+newNode = malloc(sizeof(binary_tree_t));
+if (newNode == NULL)
+return (NULL);
+newNode->n = value;
+newNode->parent = parent;
+newNode->left = newNode->right;
+if (newNode->left == NULL)
+{
+newNode->left = newNode;
+}
+	newNode->right = NULL;
+	return (newNode);
+}
