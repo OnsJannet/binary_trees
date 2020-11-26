95da819c8a17129f8660d4047257f4c8e2333b5c
onsjannet <onsjannet@gmail.com>
2020-11-26 21:47:11 +0100

    added file

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,7 +10,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 	binary_tree_postorder(tree->left, func);
-	func(tree->n);
 	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 	}
 }
