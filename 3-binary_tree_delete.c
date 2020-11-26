a59098dc3984081fa45d60e1f09accb0b1f603a1
onsjannet <onsjannet@gmail.com>
2020-11-26 19:14:09 +0100

    added 3-binary_tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete -  deletes the tree
+ * @tree: our tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
