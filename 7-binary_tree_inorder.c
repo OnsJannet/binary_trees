1e92b6a89b0199c89772875d625624aa703cf69c
onsjannet <onsjannet@gmail.com>
2020-11-26 21:05:59 +0100

    added a file

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Checks if a node is a root
+ * binary_tree_inorder - Checks if a node is a root
  * @func: Pointer to the function
  * @tree: our tree
  * Return: 1 if root, 0 if not
@@ -9,8 +9,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-	func(tree->n);
 	binary_tree_inorder(tree->left, func);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 	}
 }
