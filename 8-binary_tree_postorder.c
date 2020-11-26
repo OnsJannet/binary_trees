625ebe64bb08df9fd52e9ebb0bbbadc9efb77d23
onsjannet <onsjannet@gmail.com>
2020-11-26 21:23:43 +0100

    added a file

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Checks if a node is a root
+ * @func: Pointer to the function
+ * @tree: our tree
+ * Return: 1 if root, 0 if not
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+	binary_tree_postorder(tree->left, func);
+	func(tree->n);
+	binary_tree_postorder(tree->right, func);
+	}
+}
